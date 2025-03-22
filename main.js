61cd3b7ea0691433671a971e8bf7156a2c87a2b4
Esteban Aranguri <estebanaranguri11@gmail.com>
2025-03-22 14:07:03 -0300

    adding pause play muted and unmuted with JS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* MUSIC PLAYER ICONS */
 document.getElementById('play-music').addEventListener('click', function() {
     document.getElementById('pause-music').style.display = 'block';
     document.getElementById('play-music').style.display = 'none';
@@ -7,17 +8,31 @@ document.getElementById('play-music').addEventListener('click', function() {
 document.getElementById('pause-music').addEventListener('click', function() {
     document.getElementById('play-music').style.display = 'block';
     document.getElementById('pause-music').style.display = 'none';
-
+    
 })
+
+/* MUTE-UNMUTED AUDIO */
 
 document.getElementById('volume-mute').addEventListener('click', function() {
     document.getElementById('volume-high').style.display = 'block';
     document.getElementById('volume-mute').style.display = 'none';
+    document.getElementById("audio-showcase").muted = false;
 
 })
 
 document.getElementById('volume-high').addEventListener('click', function() {
     document.getElementById('volume-mute').style.display = 'block';
     document.getElementById('volume-high').style.display = 'none';
-
+    document.getElementById("audio-showcase").muted = true;
 })
+
+/* PLAY-PAUSE AUDIO */
+var x = document.getElementById("audio-showcase");
+
+function playAudio() {
+  x.play();
+}
+
+function pauseAudio() {
+  x.pause();
+}
